d707f14096b2f6fcab3ae1f4793ba7b6999e8e4a
matheusmarks <matheus-marques@LAPTOP-K062DHUE.localdomain>
2020-10-20 23:14:58 -0300

    first: first commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,77 @@
+import React, { useCallback, useRef, useContext } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import { AuthContext } from '../../context/AuthContext';
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+import Input from '../../Components/input/index';
+import Button from '../../Components/button/index';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useContext(AuthContext);
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Go Barber" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+          <Input name="email" type="text" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            type="password"
+            icon={FiLock}
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,14 @@
+import { ValidationError } from 'yup';
+
+interface Error {
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Error {
+  const validationErrors: Error = {};
+
+  err.inner.forEach(error => {
+    validationErrors[error.path] = error.message;
+  });
+  return validationErrors;
+}
